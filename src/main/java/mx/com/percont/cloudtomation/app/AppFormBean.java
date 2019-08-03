495ef8180c4c9b37d6df393be2ed88f6ff291781
Alex Percont <alexpercont@gmail.com>
2019-08-03 14:27:28 -0500

    System under test initial version (#2)
    * Setup of Maven dependencies for web application.
    
    * Initial version of system under test.

---
+++ src/main/java/mx/com/percont/cloudtomation/app/AppFormBean.java
@@ -0,0 +1,42 @@
+package mx.com.percont.cloudtomation.app;
+
+import static javax.faces.application.FacesMessage.*;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named("appForm")
+@RequestScoped
+public class AppFormBean {
+
+  private String regex;
+
+  private String text;
+
+  public String getRegex() {
+    return regex;
+  }
+
+  public void setRegex(String regex) {
+    this.regex = regex;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public String submit() {
+    FacesContext context = FacesContext.getCurrentInstance();
+    FacesMessage facesMessage = new FacesMessage(SEVERITY_ERROR,
+        "Not yet implemented",
+        "Implementation is yet to be developed.");
+    context.addMessage(null, facesMessage);
+    return null;
+  }
+}
