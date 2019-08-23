5755cc73f6c739d48f7fb82fdaf7ca1443813fd0
Alex Percont <alexpercont@gmail.com>
2019-08-23 08:47:59 -0500

    Cluster setup (WIP)

--- iaac/cloudtomation.tf
+++ iaac/cloudtomation.tf
@@ -12,4 +12,21 @@ provider "google" {
   region = "${var.default_region}"
 }
 
+data "google_container_registry_image" "selenium-hub-image" {
+  name = "selenium-hub"
+}
+
+resource "google_container_cluster" "selenium-hub-cluster" {
+  name = "selenium-hub-cluster"
+  initial_node_count = 1
+  remove_default_node_pool = true
+  location = "us-central1-f"
+}
+
+resource "google_container_node_pool" "selenium-hub-pool" {
+  name = "selenium-hub-pool"
+  location = "us-central1-f"
+  cluster = "${google_container_cluster.selenium-hub-cluster.name}"
+  node_count = 1
+}
 
