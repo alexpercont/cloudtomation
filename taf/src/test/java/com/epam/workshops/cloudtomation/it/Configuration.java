df68119095028b5cb96e8c3d871003e73f2a6fc0
Alex Percont <alexpercont@gmail.com>
2020-01-17 20:23:16 -0600

    Enable configuration to load properties from environment.

--- taf/src/test/java/com/epam/workshops/cloudtomation/it/Configuration.java
+++ taf/src/test/java/com/epam/workshops/cloudtomation/it/Configuration.java
@@ -3,22 +3,36 @@ package com.epam.workshops.cloudtomation.it;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.Set;
 
 public class Configuration {
 
+    private static final String TEST_PROPERTIES = "/test.properties";
     private Properties properties;
 
     public Configuration() throws IOException {
-        InputStream istream = getClass().getResourceAsStream("/test.properties");
+        loadPropertiesFile();
+        loadSystemProperties();
+    }
+
+    private void loadPropertiesFile() throws IOException {
+        InputStream istream = getClass().getResourceAsStream(TEST_PROPERTIES);
         properties = new Properties();
         properties.load(istream);
     }
 
-    public Properties getProperties(){
-        return this.properties;
+    private void loadSystemProperties(){
+        Set<String> names = properties.stringPropertyNames();
+        names.parallelStream().forEach((name) -> {
+            String systemProperty = System.getProperty(name, null);
+            if (null != systemProperty) {
+                properties.setProperty(name, systemProperty);
+            }
+        });
     }
 
     public String getProperty(String key){
         return this.properties.getProperty(key, null);
     }
+
 }
